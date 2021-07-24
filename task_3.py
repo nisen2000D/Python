1aeca5e76338631221d6adfc6c9dba1d5fbb0376
Sourcery AI <>
2021-07-24 11:17:15 +0000

    'Refactored by Sourcery'

--- task_3.py
+++ task_3.py
@@ -2,5 +2,5 @@ num = int(input('Введите число: '))
 m = 0
 while num > 0:
     m = m * 10 + num % 10
-    num = num//10
+    num //= 10
 print(m)
--- task_7.py
+++ task_7.py
@@ -1,6 +1,4 @@
 n = int(input('Введие число: '))
-s = 0
-for i in range(1, n+1):
-    s += i
+s = sum(range(1, n+1))
 m = round(n * (n + 1) / 2)
 print(f'{s}={m}')
