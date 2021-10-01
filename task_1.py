64d556b04370aefeacfb915ef8f9552d031a6c75
Dasha <nisen2000@mail.ru>
2021-10-01 17:37:09 +0400

    lesson_1

--- task_1.py
+++
@@ -1,9 +0,0 @@
-user_str = str(input("Введите строку: "))
-
-print(f'Длина строки "{user_str}": {len(user_str)} символов')
-
-subs_set = set()
-for i in range(len(user_str)):
-    for j in range(len(user_str) - 1 if i == 0 else len(user_str), i, -1):
-        subs_set.add(hash(user_str[i:j]))
-print(f'Количество подстрок: {len(subs_set)}')
