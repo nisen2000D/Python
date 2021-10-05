26a8681cb7457694239d85265f5385790e78047b
Dasha <nisen2000@mail.ru>
2021-10-05 20:40:10 +0400

    lesson_2

--- task_3.py
+++
@@ -1,6 +0,0 @@
-num = int(input('Введите число: '))
-m = 0
-while num > 0:
-    m = m * 10 + num % 10
-    num = num//10
-print(m)
--- task_5.py
+++
@@ -1,4 +0,0 @@
-for i in range(32, 128):
-    print(f'{i} - {chr(i)}')
-    if i % 10 == 0:
-        print()
